4f09e940aa548be247c73f6ba84f541d1f482072
l8te <ely.eels@gmail.com>
2020-12-24 18:46:31 -0500

    creates basic file structure

---
+++ .eleventy.js
@@ -0,0 +1,32 @@
+const fs = require("fs");
+
+const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
+
+module.exports = function(eleventyConfig) {
+  eleventyConfig.setDataDeepMerge(true);
+  eleventyConfig.addPlugin(eleventyNavigationPlugin);
+
+  // remember to add css passthrough once we have some
+
+  // Output directory: _site
+  // Copy `assets/` to `_site/assets`
+  eleventyConfig.addPassthroughCopy("assets");
+
+
+  // 404 page config
+  eleventyConfig.setBrowserSyncConfig({
+    callbacks: {
+      ready: function(err, bs) {
+
+        bs.addMiddleware("*", (req, res) => {
+          const content_404 = fs.readFileSync('_site/404.html');
+          // Add 404 http status code in request header.
+          res.writeHead(404, { "Content-Type": "text/html; charset=UTF-8" });
+          // Provides the 404 content without redirect.
+          res.write(content_404);
+          res.end();
+        });
+      }
+    }
+  });
+};
---
+++ index.md
@@ -0,0 +1,9 @@
+---
+layout: main
+title: index
+eleventyNavigation:
+	key: home
+	order: 1
+---
+
+Sub-orbital paranoid cartel assault skyscraper dolphin tiger-team tube drone singularity sign decay sprawl. Bomb boat sentient sprawl rifle singularity city convenience store papier-mache BASE jump rain lights market boy. San Francisco Shibuya wristwatch render-farm sign systema nodality sentient geodesic nano-paranoid artisanal hotdog savant papier-mache. Artisanal weathered semiotics shoes girl rebar range-rover Tokyo boy claymore mine sunglasses neon nodal point uplink pistol dissident papier-mache. Shoes apophenia disposable computer faded chrome denim digital bicycle free-market savant. 
